55de7da08fba71585b0e4f8e9f555af0e508ea89
lujy <lujingyu614@163.com>
2019-07-25 17:54:48 +0800

    modify

--- temerament.py
+++ temerament.py
@@ -1,6 +1,6 @@
 def getdivider(k):
 	d = 0;
-	for i in range(1, 10, 1):
+	for i in range(1, 100, 1):
 		if pow(2, i) >= k:
 			d = int(pow(2, i - 1))
 			break;
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 	two = 2
 	
 	q = pow(2, base / d)
-	l12 = [round(base * pow(q, n), 4) for n in range(d + 1)]
+	l12_0 = [round(base * pow(q, n), 4) for n in range(d + 1)]
 	
 	five = 3/2
 	four = 4/3
@@ -24,17 +24,32 @@ if __name__ == '__main__':
 	l5.append(four)
 	l5.sort()
 		
-	l12_5 = [l12[0], l12[2], l12[4], l12[5], l12[7], l12[9], l12[11], l12[12]]
+	l12_0_5 = [l12_0[0], l12_0[2], l12_0[4], l12_0[5], l12_0[7], l12_0[9], l12_0[11], l12_0[12]]
 	
-	derr = [abs(l5[i]- l12_5[i]) for i in range(len(l5))]
-
+	derr = [abs(l5[i]- l12_0_5[i]) for i in range(len(l5))]
+	
 	figure(1)
-	plot(l12_5)
+	plot(l12_0_5)
 	plot(l5, "r*")
 	
 	figure(2)
 	plot(derr)
+
 	
+	l12_5 = [round(pow(five, n) / getdivider(pow(five, n)), 4) for n in range(1, 12, 1)]
+	
+	l12_5.append(base)
+	l12_5.append(two)
+	l12_5.sort()
+	
+	derr = [abs(l12_0[i]- l12_5[i]) for i in range(len(l12_0))]
+
+	figure(3)
+	plot(l12_0)
+	plot(l12_5, 'ro')
+	
+	figure(4)
+	plot(derr)
 	show()
-	
+
 	
