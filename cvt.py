eeddf51f8d15dbd03fa88515803addcb4e7a2b10
lujy <lujingyu614@163.com>
2020-03-09 14:30:32 +0800

    optimise cvt.py accept console args

--- cvt.py
+++ cvt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#---lujy 2020.3---
 
 def endWith(file, ends):
     for end in ends:
@@ -31,18 +32,64 @@ def _convert(file, inEnc, outEnc):
     except IOError as err:
         print("I/O error: {0}".format(err))
 
-
-if __name__ == '__main__':
-    path = r'C:\Users\lujy\Desktop\utf8'
-    flist = getFile(path, [r'.txt'])
-    #path = r'y:\ms'
-    #flist = getFile(path, [r'.cc', r'.h', r'.cpp', r'.c'])
-    print(flist)
-    
+def convert(path, fileType, dstEnc='UTF-8'):
     import chardet
+    suffixDict = {
+        'c':(r'.cc', r'.h', r'.cpp', r'.c'),
+        'py':(r'.py'),
+        'm':(r'.m'),
+        'txt':(r'txt')
+    }
+    flist = getFile(path, suffixDict[fileType])
     for file in flist:
         with open(file, 'rb') as f:
             data = f.read()
             codeType = chardet.detect(data)['encoding']
-            _convert(file, codeType, 'UTF-8')
+            _convert(file, codeType, dstEnc)
+
+
+def showHelp():
+    print('Usage: cvt -o [dst_encode] -t [source type] --path=[source path]')
+    print('source type:')
+    print('c: c/c++ files')
+    print('py：python files')
+    print('m: matlab files')
+    print('txt: text files')
+
+
+if __name__ == '__main__':
+    import chardet
+    import sys
+    import getopt
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "-h-o-t:-p:", ['help', 'path='])
+    except getopt.GetoptError as err:
+        print(str(err))
+        showHelp()
+        sys.exit()
+    
+    path=''
+    dstEnc = 'UTF-8'
+    fileType=''
+    
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            showHelp()
+            sys.exit()
+        elif opt in ('-o'):
+            dstEnc = arg
+        elif opt in ('-t'):
+            fileType = arg
+        elif opt in ('-p', '--path'):
+            path = arg
+        else:
+            assert False, 'Unhandled Option'
             
+    if path == '' or fileType == '':
+        showHelp()
+        sys.exit()
+    
+    convert(path, fileType, dstEnc)
+    
+    sys.exit()
