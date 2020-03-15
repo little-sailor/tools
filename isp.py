41f1ed490ee6f38dca0bda6d3029d6ce39f96387
lujy <lujingyu614@163.com>
2020-03-15 20:42:15 +0800

    apply sharpen clahe

--- isp.py
+++ isp.py
@@ -1,13 +1,14 @@
-#from matplotlib.pyplot import figure, imshow, show, subplot, imsave
-import numpy as np
+# from matplotlib.pyplot import figure, imshow, show, subplot, imsave
 import struct
+
 import cv2 as cv
+import numpy as np
 
 
 def load_raw_image(file, w, h, bit_width):
     with open(file, "rb") as f:
         data = f.read()
-        data = struct.unpack('<'+str(int(len(data)/ 2))+'H', data)
+        data = struct.unpack('<' + str(int(len(data) / 2)) + 'H', data)
     raw = np.array(data).reshape(h, w)
     print(max(raw.ravel()))
     raw = raw / (1 << bit_width)
@@ -17,7 +18,7 @@ def load_raw_image(file, w, h, bit_width):
 def raw_dump(raw, file):
     import pandas as pd
     df = pd.DataFrame(raw)
-    #df.to_csv(file)
+    # df.to_csv(file)
 
 
 def wb(raw, format):
@@ -53,7 +54,7 @@ def wb(raw, format):
     subplot(2,2,4)
     imshow(r, cmap= 'gray')
     '''
-    
+
     r2g = sum(r.ravel()) / ((sum(gr.ravel()) + sum(gb.ravel())) / 2)
     b2g = sum(b.ravel()) / ((sum(gr.ravel()) + sum(gb.ravel())) / 2)
 
@@ -83,46 +84,73 @@ def wb(raw, format):
     return wb
 
 
-def bayerNR(raw, ksize, sigmaX, sigmaY):
-    # raw[::2, ::2] = cv.GaussianBlur(raw[::2, ::2], ksize, sigmaX, sigmaY)
-    # raw[::2, 1::2] = cv.GaussianBlur(raw[::2, 1::2], ksize, sigmaX, sigmaY)
-    # raw[1::2, ::2] = cv.GaussianBlur(raw[1::2, ::2], ksize, sigmaX, sigmaY)
-    # raw[1::2, 1::2] = cv.GaussianBlur(raw[1::2, 1::2], ksize, sigmaX, sigmaY)
-
-    raw[::2, ::2] = cv.bilateralFilter(raw[::2, ::2],  ksize, sigmaX, sigmaY)
-    raw[::2, 1::2] = cv.bilateralFilter(raw[::2, 1::2],  ksize, sigmaX, sigmaY)
-    raw[1::2, ::2] = cv.bilateralFilter(raw[1::2, ::2],  ksize, sigmaX, sigmaY)
-    raw[1::2, 1::2] = cv.bilateralFilter(raw[1::2, 1::2], ksize, sigmaX, sigmaY)
+def bayerNR(raw, ksize):
+    sigma = 0.3 * ((ksize - 1) * 0.5 - 1) + 0.8
+    kernel = cv.getGaussianKernel(ksize, sigma)
+    raw[::2, ::2] = cv.filter2D(raw[::2, ::2], -1, kernel)
+    raw[::2, 1::2] = cv.filter2D(raw[::2, 1::2], -1, kernel)
+    raw[1::2, ::2] = cv.filter2D(raw[1::2, ::2], -1, kernel)
+    raw[1::2, 1::2] = cv.filter2D(raw[1::2, 1::2], -1, kernel)
     return raw
 
-def sharpen(img, k=1):
-    blur_img = cv.GaussianBlur(img[:,:,0], (0, 0), 5)
-    usm = cv.addWeighted(img[:,:,0], 1.5, blur_img, -0.5, 0)
-    img[:,:,0] = usm
+
+# def bayerNR(raw, ksize, sigmaX, sigmaY):
+#     # raw[::2, ::2] = cv.GaussianBlur(raw[::2, ::2], ksize, sigmaX, sigmaY)
+#     # raw[::2, 1::2] = cv.GaussianBlur(raw[::2, 1::2], ksize, sigmaX, sigmaY)
+#     # raw[1::2, ::2] = cv.GaussianBlur(raw[1::2, ::2], ksize, sigmaX, sigmaY)
+#     # raw[1::2, 1::2] = cv.GaussianBlur(raw[1::2, 1::2], ksize, sigmaX, sigmaY)
+#
+#     raw[::2, ::2] = cv.bilateralFilter(raw[::2, ::2],  ksize, sigmaX, sigmaY)
+#     raw[::2, 1::2] = cv.bilateralFilter(raw[::2, 1::2],  ksize, sigmaX, sigmaY)
+#     raw[1::2, ::2] = cv.bilateralFilter(raw[1::2, ::2],  ksize, sigmaX, sigmaY)
+#     raw[1::2, 1::2] = cv.bilateralFilter(raw[1::2, 1::2], ksize, sigmaX, sigmaY)
+#     return raw
+
+def sharpenUSM(img, w=0.6):
+    blur_img = cv.GaussianBlur(img[:, :, 0], (0, 0), 5)
+    usm = cv.addWeighted(img[:, :, 0], 1 + w, blur_img, -w, 0)
+    img[:, :, 0] = usm
     return img
-    # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    # y = cv.filter2D(img[:,:,0], -1,kernel)
-    # s = img
-    # s[:,:,0] = y
-    # return s
+
+
+def sharpenFilter(img, whiteEdge=0.1, blackEdge=-0.1):
+    kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    y0 = img[:, :, 0]
+
+    edge = cv.filter2D(y0, cv.CV_16S, kernel)
+
+    y0[y0 == 0] = 1
+    edge = edge / y0
+
+    # th0 = 0.8
+    edge[edge > whiteEdge] = whiteEdge
+    edge[edge < blackEdge] = blackEdge
+    # edge[np.abs(edge) < th0] = 0
+
+    enhance = y0 + y0 * edge
+
+    enhance = np.clip(enhance, 0, 255).astype('uint8')
+
+    img[:, :, 0] = enhance
+    return img
 
 
 def demosaic(raw, format):
-    bayerFormat = {'rggb':cv.COLOR_BayerRG2RGB,
-                   'bggr':cv.COLOR_BayerBG2RGB,
-                   'grbg':cv.COLOR_BayerGR2RGB,
-                   'gbrg':cv.COLOR_BayerGB2RGB
+    bayerFormat = {'rggb': cv.COLOR_BayerRG2RGB,
+                   'bggr': cv.COLOR_BayerBG2RGB,
+                   'grbg': cv.COLOR_BayerGR2RGB,
+                   'gbrg': cv.COLOR_BayerGB2RGB
                    }
     rgb = cv.cvtColor(raw, bayerFormat[format])
     return rgb
 
 
 def gamma(rgb, g):
-    return np.power(rgb, 1/g)
+    return np.power(rgb, 1 / g)
 
 
 def gammaRGB(img, bitw, g):
-    return (np.power(img / (1 << 16),  1/g) * 255).astype('uint8')
+    return np.clip((np.power(img / (1 << 16), 1 / g) * 256), 0, 255).astype('uint8')
 
 
 def rgb2yuv(img):
@@ -134,8 +162,12 @@ def blc(raw, ob):
     new[new < 0] = 0
     return new
 
+def claheApply(img):
+    clahe = cv.createCLAHE(2, (4, 4))
+    img[:, :, 0] = clahe.apply(img[:, :, 0])
+    return img
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     w = 1920
     h = 1080
     bitwitdh = 12
@@ -168,31 +200,30 @@ if __name__ == '__main__' :
 
     img = blc(img, ob / (1 << bitwitdh))
 
-    img = bayerNR(img, 2, 75, 75)
+    img = bayerNR(img, 1)
 
-    img = gamma(img, 1.6)
+    img = gamma(img, 1.2)
 
     img = wb(img, bayerFormat)
 
-
-
     # fix domain 16bit
-    img = (img * 65535).astype('uint16')
+    img = np.clip((img * 65536), 0, 65535).astype('uint16')
     img = demosaic(img, bayerFormat)
 
     # ccm
 
     # gamma rgb
-    img = gammaRGB(img, bitwitdh, 3.5)
+    img = gammaRGB(img, bitwitdh, 1.5)
 
     # rgb 2 yuv
     img = rgb2yuv(img)
 
     # sharpen
-    img = sharpen(img)
+    #img = sharpenUSM(img, 0.99)
+    # img = sharpenFilter(img, 0.8, -0.4)
 
     # csc
-
+    claheApply(img)
 
     # show and save
     imgf = cv.cvtColor(img, cv.COLOR_YUV2RGB)
